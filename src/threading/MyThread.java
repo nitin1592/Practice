45c9680fd19dc2256625dbecedfe74c1e9dafe8b
nitin <nitinbansal1592@gmail.com>
2020-08-14 00:36:46 +0530

    misc

--- src/threading/MyThread.java
+++ src/threading/MyThread.java
@@ -1,8 +1,15 @@
 package threading;
 
 public class MyThread extends Thread{
+	MyThread() {
+		System.out.println("My thread");
+	}
 	public void run() {
-		System.out.println("Thread is " + Thread.currentThread().getName());
+		System.out.println("run ");
+	}
+	
+	public void run(String s) {
+		System.out.println("run with para");
 	}
 
 }
